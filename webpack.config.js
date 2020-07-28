7ea653b9d7098080434025e6993a680678025f78
Bryan Rodriguez <bsteven1993@gmail.com>
2020-07-27 19:57:06 -0500

    se agrega ruta y vista para login

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,9 @@ module.exports = {
             }
         ]
     },
+    devServer: {
+        historyApiFallback: true,
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: './public/index.html',
