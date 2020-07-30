e88562706e3b45ac0c043d86b9c1f5b17c42919a
Bryan Rodriguez <bsteven1993@gmail.com>
2020-07-30 07:30:19 -0500

    se agrega funcionalidad de logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import '../assets/styles/components/Header.scss';
-
-import gravatar from '../utils/gravatar';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import usericon from '../assets/static/user-icon.png';
+
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 
 const Header = ( props ) => {
 
     const { user } = props;
     console.log(user.email);
     const hasUser = Object.keys(user).length > 0;
+
+    const handleLogout = () => {
+        props.logoutRequest({});
+    }
     
     return (
 
@@ -30,8 +35,17 @@ const Header = ( props ) => {
                 <p>Perfil</p>
             </div>
             <ul>
-                <li><a href="/">Cuenta</a></li>
-                <li><Link to="/login">Cerrar Sesión</Link></li>
+                {hasUser ?
+                    <li><a href="/">{user.email}</a></li>
+                    :
+                    null
+                }
+                {hasUser ? 
+                    <li><a href="#logout" onClick={handleLogout}>Cerrar sesión</a></li>
+                    :
+                    <li><Link to="/login">Iniciar sesión</Link></li>
+
+                }
             </ul>
             </div>
         </header>
@@ -45,5 +59,9 @@ const mapStateToProps = state => {
     }
 }
 
+const mapDispatchToProps = {
+    logoutRequest
+}
+
 // export default Header;
-export default connect(mapStateToProps, null)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
